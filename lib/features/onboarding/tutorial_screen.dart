10a129498ea94cbe37fc296a16dc1059ca4f0625
hwasub <hwasub1115@naver.com>
2025-03-15 16:21:44 +0900

    5.4 AnimationedCrossFade

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -1,8 +1,131 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
 
+enum Direction { right, left }
+
+enum Page { first, second }
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showinPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showinPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showinPage = Page.first;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onPanEnd: _onPanEnd,
+      onPanUpdate: _onPanUpdate,
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+          child: SafeArea(
+            child: AnimatedCrossFade(
+              firstChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  Text(
+                    "Watch cool videos!",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Videos are personalized for  you based on what you watch, like, and share.",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+              secondChild: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gaps.v80,
+                  Text(
+                    "Follow the rules",
+                    style: TextStyle(
+                      fontSize: Sizes.size40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Gaps.v16,
+                  Text(
+                    "Take care of on another. Plis!",
+                    style: TextStyle(
+                      fontSize: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+              crossFadeState: _showinPage == Page.first
+                  ? CrossFadeState.showFirst
+                  : CrossFadeState.showSecond,
+              duration: Duration(milliseconds: 300),
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          height: 130,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: Sizes.size24,
+              horizontal: Sizes.size24,
+            ),
+            child: AnimatedOpacity(
+              duration: Duration(microseconds: 300),
+              opacity: _showinPage == Page.first ? 0 : 1,
+              child: CupertinoButton(
+                  color: Theme.of(context).primaryColor,
+                  child: Text(
+                    "Enter the app!",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {}),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
 
@@ -116,3 +239,4 @@ class _TutorialScreenState extends State<TutorialScreen> {
     );
   }
 }
+*/
