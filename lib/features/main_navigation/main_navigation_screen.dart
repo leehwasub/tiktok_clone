cd223bf53cf9441eedcf40fcc556b3370583cdaf
hwasub <hwasub1115@naver.com>
2025-03-16 17:29:53 +0900

    6.7 Stateful Navigation

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/features/main_navigation/stf_screen.dart';
 
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
@@ -16,24 +17,6 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    Center(
-      child: Text("Home"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-    Center(
-      child: Text("Home"),
-    ),
-    Center(
-      child: Text("Search"),
-    ),
-    Center(
-      child: Text("Home"),
-    ),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
@@ -44,6 +27,26 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: StfScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 4,
+            child: StfScreen(),
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         height: 90,
         color: Colors.black,
@@ -56,24 +59,28 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 text: "Home",
                 isSelected: _selectedIndex == 0,
                 icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
               ),
               NavTab(
                 text: "Discover",
                 isSelected: _selectedIndex == 1,
-                icon: FontAwesomeIcons.magnifyingGlass,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
               NavTab(
                 text: "Inbox",
                 isSelected: _selectedIndex == 3,
                 icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
               ),
               NavTab(
                 text: "Profile",
                 isSelected: _selectedIndex == 4,
                 icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
               ),
             ],
