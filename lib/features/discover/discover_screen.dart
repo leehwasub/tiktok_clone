de301f39f8885cec67f5b652009b96217682b74b
hwasub <hwasub1115@naver.com>
2025-03-20 02:40:55 +0900

    9.2 TabBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/sizes.dart';
+
 class DiscoverScreen extends StatelessWidget {
-  const DiscoverScreen({super.key});
+  DiscoverScreen({super.key});
+
+  final tabs = [
+    "Tops",
+    "Users",
+    "Videos",
+    "Sounds",
+    "LIVE",
+    "Shopping",
+    "Brands",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Discover"),
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Discover"),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: EdgeInsets.symmetric(
+              horizontal: Sizes.size16,
+            ),
+            isScrollable: true,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [
+              for (var tab in tabs)
+                Tab(
+                  text: tab,
+                ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(
+                child: Text(
+                  tab,
+                  style: TextStyle(
+                    fontSize: Sizes.size28,
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
