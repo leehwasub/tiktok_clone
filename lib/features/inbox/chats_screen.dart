e9f34d6c86e18177cb8523a52dc647d973dabf7f
hwasub <hwasub1115@naver.com>
2025-03-21 17:04:49 +0900

    11.3 AnimatedList part2

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/features/inbox/chat_detail_screen.dart';
 
 import '../../constants/sizes.dart';
 
@@ -15,11 +16,75 @@ class _ChatsScreenState extends State<ChatsScreen> {
 
   final List<int> _items = [];
 
+  final Duration _duration = Duration(
+    milliseconds: 300,
+  );
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(_items.length);
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: _duration,
+      );
       _items.add(_items.length);
     }
+  }
+
+  void _deleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(
+            color: Colors.red,
+            child: _makeTile(index),
+          ),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _deleteItem(index),
+      onTap: _onChatTap,
+      leading: CircleAvatar(
+        radius: 30,
+        foregroundImage:
+            NetworkImage("https://avatars.githubusercontent.com/u/3612017"),
+        child: Text("nico"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "Lynn ($index)",
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: Text("Don't forget to make video"),
+    );
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ChatDetailScreen(),
+      ),
+    );
   }
 
   @override
@@ -42,37 +107,11 @@ class _ChatsScreenState extends State<ChatsScreen> {
         ),
         itemBuilder: (context, index, animation) {
           return FadeTransition(
-            key: UniqueKey(),
+            key: Key("$index"),
             opacity: animation,
             child: SizeTransition(
               sizeFactor: animation,
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 30,
-                  foregroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/3612017"),
-                  child: Text("nico"),
-                ),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      "Lynn ($index)",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "2:16 PM",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
-                      ),
-                    ),
-                  ],
-                ),
-                subtitle: Text("Don't forget to make video"),
-              ),
+              child: _makeTile(index),
             ),
           );
         },
