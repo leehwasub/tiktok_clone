d9836cacf342a8c6d0ed22c992e415856b056d95
hwasub <hwasub1115@naver.com>
2025-03-23 00:05:20 +0900

    12.2 SilverAppBar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -9,9 +9,11 @@ class UserProfileScreen extends StatelessWidget {
       physics: BouncingScrollPhysics(),
       slivers: [
         SliverAppBar(
-          floating: true,
-          stretch: true,
-          pinned: true,
+          snap: true, //조금만 스크롤 내리고 터치를 해제하면 끝까지 내려옴
+          floating: true, // 스크롤 올릴때 끝까지 안올려도 바가내려움
+          stretch:
+              true, //스크롤을 끝까지 올리고나서 밑으로 더끌어당길수있는 옵션. FlexibleSpaceBar로 효과를설정
+          pinned: true, //스크롤을 올렸을때 CollapsedHeight이하로 사라지지않음
           backgroundColor: Colors.teal,
           elevation: 1,
           collapsedHeight: 80,
@@ -31,7 +33,14 @@ class UserProfileScreen extends StatelessWidget {
         ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
-            (context, index) => Container(),
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
           ),
           itemExtent: 100,
         ),
