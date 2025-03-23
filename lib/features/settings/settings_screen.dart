b0629e2b795b84f891e630a0f8d172728b41fdd2
hwasub <hwasub1115@naver.com>
2025-03-23 17:43:49 +0900

    13.3 SwitchListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNotificationChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +26,28 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          SwitchListTile.adaptive(
+            title: Text("Enable Notirications"),
+            subtitle: Text("Enable"),
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          Switch.adaptive(
+              value: _notifications, onChanged: _onNotificationChanged),
+          CupertinoSwitch(
+              value: _notifications, onChanged: _onNotificationChanged),
+          SwitchListTile(
+            title: Text("Enable Notirications"),
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+          ),
+          Checkbox(value: _notifications, onChanged: _onNotificationChanged),
+          CheckboxListTile(
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNotificationChanged,
+            title: Text("Enable notification"),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
