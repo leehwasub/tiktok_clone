0b4584e9005bf16a9105cf72af46dc3911b8abf9
hwasub <hwasub1115@naver.com>
2025-04-05 15:02:57 +0900

    25.2 UserProfileModel

--- lib/features/authentication/view_models/signup_view_model.dart
+++ lib/features/authentication/view_models/signup_view_model.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
+import 'package:tiktok_clone/features/users/view_models/users_view_model.dart';
 import 'package:tiktok_clone/utils.dart';
 
 class SignUpViewModel extends AsyncNotifier<void> {
@@ -18,11 +20,15 @@ class SignUpViewModel extends AsyncNotifier<void> {
   Future<void> signUp(BuildContext context) async {
     state = AsyncValue.loading();
     final form = ref.read(signUpForm);
+    final users = ref.read(usersProvider.notifier);
     state = await AsyncValue.guard(
-      () async => await _authRepo.signUp(
-        form["email"],
-        form["password"],
-      ),
+      () async {
+        final userCredential = await _authRepo.signUp(
+          form["email"],
+          form["password"],
+        );
+        await users.createAccount(userCredential);
+      },
     );
     if (!context.mounted) return;
 
