c8f9959e10dd093caf8bbab06bb30105bd3b8f80
hwasub <hwasub1115@naver.com>
2025-04-07 23:09:17 +0900

    29.2 Foreground notifications

---
+++ lib/features/notification/notifications_provider.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+
+class NotificationsProvider extends AsyncNotifier {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
+
+  Future<void> updateToken(String token) async {
+    final user = ref.read(authRepo).user;
+    await _db.collection("users").doc(user!.uid).update({"token": token});
+  }
+
+  Future<void> initListeners() async {
+    final permission = await _messaging.requestPermission();
+    if (permission.authorizationStatus == AuthorizationStatus.denied) {
+      return;
+    }
+    //foreground
+    FirebaseMessaging.onMessage.listen(
+      (RemoteMessage event) {
+        print("I just got a message and I'm in the foreground");
+        print(event.notification?.title);
+      },
+    );
+  }
+
+  @override
+  FutureOr build() async {
+    final token = await _messaging.getToken();
+    if (token == null) return;
+    await updateToken(token);
+    await initListeners();
+    _messaging.onTokenRefresh.listen(
+      (newToken) async {
+        await updateToken(newToken);
+      },
+    );
+  }
+}
+
+final notificationsProvider = AsyncNotifierProvider(
+  () => NotificationsProvider(),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tiktok_clone/features/notification/notifications_provider.dart';
 import 'package:tiktok_clone/features/videos/repos/video_playback_config_repo.dart';
 import 'package:tiktok_clone/features/videos/view_models/playback_config_vm.dart';
 import 'package:tiktok_clone/firebase_options.dart';
@@ -48,6 +49,7 @@ class TikTokApp extends ConsumerWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.watch(notificationsProvider);
     return MaterialApp.router(
       routerConfig: ref.watch(routerProvider),
       debugShowCheckedModeBanner: false,
